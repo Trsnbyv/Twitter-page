3fb1d10b95a2ac6b82d7d0e3f3ea9bb41bb6d2be
Trsnbyv <tursunboyevsaidkomil@gmail.com>
2024-08-06 23:42:23 +0500

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
-import {Context} from "./context/Context"
-import { useContext } from 'react'
-import Dashboard from './routes/Dashboard/Index'
-import Login from './routes/Login/Index'
+
+import './App.css';
+import { Context } from "./context/Context";
+import { useContext } from 'react';
+import Dashboard from './routes/Dashboard/Index';
+import Login from './routes/Login/Index';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-  const {token} = useContext(Context)
-  return token ? <Dashboard/> : <Login />
+  const { token } = useContext(Context);
+  return (
+    <>
+      <Toaster position="top-right" reverseOrder={false}/>
+      {token ? <Dashboard /> : <Login />}
+    </>
+  );
 }
 
-export default App
+export default App;
